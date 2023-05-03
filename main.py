dbecb8f2c9c7bdb31822b46c787ddcc4607684b0
Bazil Anandaraj <82741936+Bazil-A@users.noreply.github.com>
2023-05-03 14:33:56 -0400

    Added speak(), startup(), and getTime() functions

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from gtts import gTTS
 from playsound import playsound
-import time
+from datetime import datetime
 
 language = 'en'
 
-
+# Speaks the input string via TTS
 def speak(sentence):
     tts = gTTS(text = sentence, lang = language)
     tts.save('speech.mp3')
     playsound('speech.mp3')
 
-speak("Hello world")
-speak("My name is Synthia")
+# For initializations and startup speech
+def startup():
+    speak("Hello World! My name is Synthia!")
+
+# Retrieves the current time 
+def getTime():
+    currentTime = datetime.now().strftime("%I:%M %p")
+    speak("The current time is " + currentTime)
+
+# Main function
+def main():
+    startup()
+
+#Call main function 
+main()
